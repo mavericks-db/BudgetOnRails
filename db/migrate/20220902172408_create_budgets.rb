fb00c2b31db4d68abb28225b472750ad3f38187e
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-02 22:28:07 +0500

    Generate budget model

---
+++ db/migrate/20220902172408_create_budgets.rb
@@ -0,0 +1,11 @@
+class CreateBudgets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :budgets do |t|
+      t.references :author_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.string :name, null: false
+      t.decimal :amount, default: 0.0, null: false
+
+      t.timestamps
+    end
+  end
+end
