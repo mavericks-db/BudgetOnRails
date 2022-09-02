dfa0210f4fe5f8c2822b7a1f28e80bfc5d8490f6
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-02 23:45:02 +0500

    Initialize letter_opener_web

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p "Seeding database..."
+
+# Create 10 users
+1.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 123456, password_confirmation: 123456)
+end
+
+p "Seeding complete!"
+
+p "Created #{User.count} users"
