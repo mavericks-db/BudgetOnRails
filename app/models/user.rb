ff0874111b4ca52bc71ca8a7b95fd5e4a5c2eb93
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-02 23:30:06 +0500

    Generate devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :budgets, dependent: :destroy
   has_many :group_budgets, through: :budgets, dependent: :destroy
   has_many :groups, through: :group_budgets, dependent: :destroy
