9129c7007d596895d74ece39b8e252860d3453a9
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-04 02:02:16 +0500

    Add descriptive README.md file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     return unless user.present?
+
     can :manage, Budget, author_id: user.id
     can :manage, Group, author_id: user.id
     can :manage, GroupBudget, author_id: user.id
