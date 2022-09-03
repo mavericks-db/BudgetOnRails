99740913f89a72c0574019edfadc291558eeef60
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-03 09:31:49 +0500

    Remove defaults from models

--- db/migrate/20220902172408_create_budgets.rb
+++ db/migrate/20220902172408_create_budgets.rb
@@ -2,7 +2,7 @@ class CreateBudgets < ActiveRecord::Migration[7.0]
   def change
     create_table :budgets do |t|
       t.references :author, null: false, foreign_key: { to_table: :users }, index: true
-      t.string :name, null: false, default: "New Budget"
+      t.string :name, null: false
       t.decimal :amount, default: 0.0, null: false
 
       t.timestamps
