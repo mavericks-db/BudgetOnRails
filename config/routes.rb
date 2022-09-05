bde257fd5abb89ad650cf5a6a090da8293ab544e
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-05 08:45:33 +0500

    Create list of groups page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :users
+  resources :groups
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
