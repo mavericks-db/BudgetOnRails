961b07df5272eaf2fa1ec57c0f392fd6b3609fc8
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-05 19:43:29 +0500

    Add spec test for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get '/users' }
+
+    it 'returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns correct response body' do
+      expect(response.body).to include('Users List')
+    end
   end
 end
