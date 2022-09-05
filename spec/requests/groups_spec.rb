fed7480a56ae09382798e38c72072e69a95eaa2f
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-05 19:46:23 +0500

    Add spec test for groups

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Groups', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get '/groups' }
+
+    it 'returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns correct response body' do
+      expect(response.body).to include('Add New Category')
+    end
   end
 end
