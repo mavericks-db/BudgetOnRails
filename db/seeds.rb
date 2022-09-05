1ee7b17417100f2ae4779d944997635198e220e8
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-05 09:00:10 +0500

    Seed groups database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,25 @@ require 'faker'
 
 p "Seeding database..."
 
-# Create 10 users
-1.times do
+# Create 5 users
+5.times do
   User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 123456, password_confirmation: 123456)
 end
+
+# Create 5 groups
+Group.create(name: "Housing", icon: "house")
+Group.create(name: "Transportation", icon: "directions_car")
+Group.create(name: "Food", icon: "restaurant")
+Group.create(name: "Utilities", icon: "lightbulb")
+Group.create(name: "Insurance", icon: "emergency")
+Group.create(name: "Medical & Healthcare", icon: "local_hospital")
+Group.create(name: "Saving, Investing, & Debt Payments", icon: "attach_money")
+Group.create(name: "Personal Spending", icon: "shopping_cart")
+Group.create(name: "Recreation & Entertainment", icon: "sports_esports")
+Group.create(name: "Education", icon: "school")
+Group.create(name: "Miscellaneous", icon: "more_horiz")
 
 p "Seeding complete!"
 
 p "Created #{User.count} users"
+p "Created #{Group.count} groups"
